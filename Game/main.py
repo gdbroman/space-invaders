92b3edabbc39fca3651dc963eee3f9223d336bd8
Gustaf <99gustaf@gmail.com>
2018-10-09 16:27:45 +0200

    Shoot able

--- Game/main.py
+++ Game/main.py
@@ -28,5 +28,87 @@ player.speed(0)
 player.setposition(0, -250)
 player.setheading(90)
 
+playerspeed = 15
+
+#Create enemy
+enemy = turtle.Turtle()
+enemy.color("red")
+enemy.shape("circle")
+enemy.penup()
+enemy.speed(0)
+enemy.setposition(-200, 250)
+enemyspeed = 2
+
+#Create the player's bullet
+bullet = turtle.Turtle()
+bullet.color("yellow")
+bullet.shape("triangle")
+bullet.penup()
+bullet.setheading(90)
+bullet.shapesize(0.5, 0.5)
+bullet.hideturtle()
+
+bulletspeed = 20
+
+#Define bullet state
+#ready = ready to fire
+#fire = bullet is firing
+bulletstate = "firing"
+
+#Move the player left and right
+def move_left():
+    x = player.xcor()
+    x -= playerspeed
+    if x < -280:
+        x = -280
+    player.setx(x)
+
+def move_right():
+    x = player.xcor()
+    x += playerspeed
+    if x > 280:
+        x = 280
+    player.setx(x)
+
+def fire_bullet():
+    #Declare bulletstate as a global if it needs changed
+    global bulletstate
+
+    #Move the bullet to just above the player
+    x = player.xcor()
+    y = player.ycor() + 10
+    bullet.setposition(x, y)
+    bullet.showturtle()
+
+#Create keyboard bindings
+turtle.listen()
+turtle.onkey(move_left, "Left")
+turtle.onkey(move_right, "Right")
+turtle.onkey(fire_bullet, "space")
+
+#Main game loop
+while True:
+    wn.update()
+    #Move the enemy
+    x = enemy.xcor()
+    x += enemyspeed
+    enemy.setx(x)
+    #Move the enemy back and down
+    if enemy.xcor() > 280:
+        enemyspeed *= -1
+        y = enemy.ycor()
+        y -= 40
+        enemy.sety(y)
+    if enemy.xcor() <-280:
+        enemyspeed*= -1
+        y = enemy.ycor()
+        y -= 40
+        enemy.sety(y)
+
+    #Move the bullet
+    y = bullet.ycor()
+    y += bulletspeed
+    bullet.sety(y)
 
 turtle.mainloop()
+wn.tracer(0)
